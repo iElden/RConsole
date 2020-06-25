cb69195da1cabfbb207c00c4ca14fe270abbe1b6
iElden <mceldenn@gmail.com>
2020-06-25 12:30:37 +0200

    Added RClient

---
+++ client/RClient.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2020
+** RConsole
+** File description:
+** RClient.cpp
+*/
+#include "RClient.hpp"
+
+bool RC::RClient::isInLobby() const noexcept
+{
+	return this->myLobby.has_value();
+}
+
+RC::RClientLobby &RC::RClient::getLobby() const
+{
+	return *this->myLobby;
+}
---
+++ client/RClient.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2020
+** RConsole
+** File description:
+** RClient.hpp
+*/
+#ifndef RCONSOLE_RCLIENT_HPP
+#define RCONSOLE_RCLIENT_HPP
+
+#include <optional>
+#include "RClientLobby.hpp"
+
+namespace RC {
+	class RClient {
+		std::optional<RPlayer> &me;
+		std::optional<RClientLobby> &myLobby;
+
+		bool isInLobby() const noexcept;
+		RClientLobby& getLobby() const; // raise if not in lobby
+	};
+}
+
+
+#endif //RCONSOLE_RCLIENT_HPP
