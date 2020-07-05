543ab86ea42d7582219f4d9a070c4326d98a6c93
Thomas Bouvier <thomas.bouvier@epitech.eu>
2020-07-05 18:58:25 +0200

    Gamepad update : sockets are handled correctly and first ping-pong is correctly managed.

--- gamepad/app/src/main/java/com/rconsole/rgamepad/MainActivity.kt
+++ gamepad/app/src/main/java/com/rconsole/rgamepad/MainActivity.kt
@@ -4,24 +4,36 @@ import android.content.Context
 import android.os.Bundle
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
 
 
 class MainActivity : AppCompatActivity() {
 
     private var ip: String = ""
     private var id: Int = 0
+    private lateinit var address: InetAddress
+    private var lastID = ""
+
+    private var send: DatagramSocket? = null
+    private var receive: DatagramSocket? = null
+
 
     private fun vibrate() {
         val vibrator = this.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE))
     }
 
-    private fun initGamepadButtons() {
+    private fun initGamepadButtons(s: DatagramSocket?) {
         val buttons = arrayOf<Button>(
             findViewById(R.id.buttonA),
             findViewById(R.id.buttonB),
@@ -35,7 +47,9 @@ class MainActivity : AppCompatActivity() {
 
         for (button in buttons) {
             button.setOnTouchListener(Repeater(400, 100, View.OnClickListener {
-                SendData(ip, id).execute(AsyncParams(button.toString(), applicationContext ))
+                Thread {
+                    send(button.toString())
+                }.start()
                 vibrate()
             }))
         }
@@ -44,41 +58,105 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_activity)
-
     }
 
-    fun connectToClient(view: View) {
+    private fun receive(timeout: Int = 2000): String {
+        val buffer = ByteArray(1000)
+        val packet = DatagramPacket(buffer, 0, buffer.size)
+        receive?.soTimeout = timeout
+        receive?.receive(packet)
+        return String(buffer, 0, buffer.size)
+    }
+
+    private fun send(msg: String) {
+        val bytes = msg.toByteArray()
+        val packet = DatagramPacket(bytes, bytes.size, address, id)
+        send?.send(packet)
+    }
+
+    private fun firstSend() {
+        send("Hello")
+    }
+
+    fun err(e: Exception) {
+        val sw = StringWriter()
+        e.printStackTrace(PrintWriter(sw))
+        val exceptionAsString = sw.toString()
+        Log.e("Thread", exceptionAsString)
+    }
+
+    private fun run() {
+        val btn = findViewById<Button>(R.id.connect)
 
         val inputIp = findViewById<EditText>(R.id.clientIP).text.toString() // Client IP address
         val inputId = findViewById<EditText>(R.id.clientID).text.toString() // Client Socket
+
+
 
         if (inputIp.isNotEmpty() && inputId.isNotEmpty()) {
             ip = inputIp
             id = inputId.toInt()
 
-            SendData(ip, id, after = {
-                if (it != null) {
-                    Toast.makeText(applicationContext, it.toString(), Toast.LENGTH_SHORT).show()
-                } else {
-                    ReceiveData(id, after = {
-                        runOnUiThread {
-                            setContentView(R.layout.gamepad_activity)
-                            initGamepadButtons()
-                        }
-                    }).execute()
+            try {
+                println("Oui...")
+                if (lastID != inputId) {
+                    send?.close()
+                    receive?.close()
+                    send = DatagramSocket(id)
+                    receive = DatagramSocket(id + 1)
+                    lastID = inputId
                 }
-            }).execute(AsyncParams("Hello", applicationContext))
+
+                address = InetAddress.getByName(ip)
+
+                firstSend()
+
+                runOnUiThread {
+                    btn.isEnabled = false
+                }
+                val msg = receive()
+                runOnUiThread {
+                    btn.isEnabled = true
+                }
+
+                if (msg.startsWith("Olleh")) {
+                    runOnUiThread {
+                        setContentView(R.layout.gamepad_activity)
+                        initGamepadButtons(send)
+                    }
+                } else {
+                    throw RException("Bad response, try an other client.")
+                }
+
+            } catch (e: Exception) {
+                err(e)
+                runOnUiThread {
+                    Toast.makeText(applicationContext, e.toString(), Toast.LENGTH_SHORT).show()
+                    btn.isEnabled = true
+                }
+            }
         } else {
-            Toast.makeText(
-                applicationContext,
-                "You must enter the client IP and ID.",
-                Toast.LENGTH_LONG
-            ).show()
+            runOnUiThread {
+                Toast.makeText(
+                    applicationContext,
+                    "You must enter the client IP and ID.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
     }
 
+    fun connectToClient(view: View) {
+        try {
+            Thread {
+                run()
+            }.start()
+        } catch (e: Exception) {}
+    }
+
     override fun onStop() {
-        SendData(ip, id).execute(AsyncParams("Stop", applicationContext))
+        send?.close()
+        receive?.close()
         super.onStop()
     }
 }
