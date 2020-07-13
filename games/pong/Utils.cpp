2b88b7765ed56e0fbf7ab57b7fe23159cc7ca5ca
iElden <mceldenn@gmail.com>
2020-07-13 19:21:09 +0200

    Started Pong Game

---
+++ games/pong/Utils.cpp
@@ -0,0 +1,7 @@
+/*
+** EPITECH PROJECT, 2020
+** RConsole
+** File description:
+** Utils.cpp
+*/
+#include "Utils.hpp"
---
+++ games/pong/Utils.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2020
+** RConsole
+** File description:
+** Utils.hpp
+*/
+#ifndef RCONSOLE_UTILS_HPP
+#define RCONSOLE_UTILS_HPP
+
+
+namespace RC::Pong {
+	enum Direction1D {
+		NONE,
+		UP,
+		DOWN
+	};
+};
+
+
+#endif //RCONSOLE_UTILS_HPP
