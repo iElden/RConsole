bc1de25718324c246d11cce878e3f702b56f11d6
Thomas Bouvier <thomas.bouvier@epitech.eu>
2020-07-13 16:23:24 +0200

    Draft for mobile <-> client protocol implementation.

--- gamepad/app/src/main/java/com/rconsole/rgamepad/AsyncParams.kt
+++
@@ -1,5 +0,0 @@
-package com.rconsole.rgamepad
-
-import android.content.Context
-
-class AsyncParams internal constructor(var msg: String, var context: Context?)
--- gamepad/app/src/main/java/com/rconsole/rgamepad/MainActivity.kt
+++ gamepad/app/src/main/java/com/rconsole/rgamepad/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -15,6 +16,9 @@ import java.io.StringWriter
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
+import kotlin.experimental.and
+import kotlin.experimental.or
+import kotlin.experimental.xor
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,35 +28,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var address: InetAddress
     private var lastID = ""
 
-    private var send: DatagramSocket? = null
-    //private var receive: DatagramSocket? = null
-
+    private var socket: DatagramSocket? = null
 
     private fun vibrate() {
         val vibrator = this.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE))
-    }
-
-    private fun initGamepadButtons(s: DatagramSocket?) {
-        val buttons = arrayOf<Button>(
-            findViewById(R.id.buttonA),
-            findViewById(R.id.buttonB),
-            findViewById(R.id.buttonX),
-            findViewById(R.id.buttonY),
-            findViewById(R.id.buttonUp),
-            findViewById(R.id.buttonDown),
-            findViewById(R.id.buttonLeft),
-            findViewById(R.id.buttonRight)
-        )
-
-        for (button in buttons) {
-            button.setOnTouchListener(Repeater(400, 100, View.OnClickListener {
-                Thread {
-                    send(button.toString())
-                }.start()
-                vibrate()
-            }))
-        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,31 +40,74 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.register_activity)
     }
 
-    private fun receive(timeout: Int = 2000): String {
-        val buffer = ByteArray(1000)
+    private fun receive(timeout: Int = 2000): ByteArray {
+        val buffer = ByteArray(2)
         val packet = DatagramPacket(buffer, 0, buffer.size)
-        //receive?.soTimeout = timeout
-        send?.soTimeout = timeout
-        //receive?.receive(packet)
-        send?.receive(packet)
-        return String(buffer, 0, buffer.size)
+        socket?.soTimeout = timeout
+        socket?.receive(packet)
+        return buffer
     }
 
-    private fun send(msg: String) {
-        val bytes = msg.toByteArray()
-        val packet = DatagramPacket(bytes, bytes.size, address, id)
-        send?.send(packet)
+    private fun send(packet: Packet) {
+        var msg = ByteArray(1)
+        msg[0] = packet.opcode
+        msg = msg.plus(packet.data)
+        socket?.send(DatagramPacket(msg, msg.size, address, id))
     }
 
     private fun firstSend() {
-        send("Hello")
+        send(Packet(0.toByte()))
     }
 
-    fun err(e: Exception) {
+    private fun err(e: Exception) {
         val sw = StringWriter()
         e.printStackTrace(PrintWriter(sw))
         val exceptionAsString = sw.toString()
         Log.e("Thread", exceptionAsString)
+    }
+
+
+    fun same(byte: Byte, mask: Byte): Boolean {
+        return byte.and(mask) == mask
+    }
+
+    private fun gamepadLoop() {
+
+        Thread {
+            var packet = ByteArray(14)
+            val buttons = arrayOf<Button>(
+                findViewById(R.id.buttonA),
+                findViewById(R.id.buttonB),
+                findViewById(R.id.buttonX),
+                findViewById(R.id.buttonY),
+                findViewById(R.id.buttonUp),
+                findViewById(R.id.buttonDown),
+                findViewById(R.id.buttonLeft),
+                findViewById(R.id.buttonRight)
+            )
+
+            for (button in buttons) {
+                button.setOnTouchListener { view, me ->
+                    packet[0] = packet[0].or(button.tag.toString().toInt().toByte())
+                    return@setOnTouchListener true
+                }
+            }
+            while (true) {
+                try {
+                    var msg = receive(10000)
+
+                    if (same(msg[0], 0x02)) {
+                        if (same(msg[1], 0x00)) {
+                            vibrate() // Have to play with bit manipulation.
+                        }
+                    }
+                    send(Packet(3.toByte(), packet))
+                    packet = ByteArray(14)
+                } catch (e: Exception) {
+                    err(e)
+                }
+            }
+        }.start()
     }
 
     private fun run() {
@@ -94,15 +117,13 @@ class MainActivity : AppCompatActivity() {
         val inputId = findViewById<EditText>(R.id.clientID).text.toString() // Client Socket
 
         if (inputIp.isNotEmpty() && inputId.isNotEmpty()) {
-            ip = inputIp
-            id = inputId.toInt()
-
             try {
-                if (lastID != inputId) {
-                    send?.close()
-                    //receive?.close()
-                    send = DatagramSocket(id)
-                    //receive = DatagramSocket(id + 1)
+                ip = inputIp
+                id = inputId.toInt()
+
+                if (lastID != inputId || socket?.isClosed!!) {
+                    socket?.close()
+                    socket = DatagramSocket(id)
                     lastID = inputId
                 }
 
@@ -117,11 +138,10 @@ class MainActivity : AppCompatActivity() {
                 runOnUiThread {
                     btn.isEnabled = true
                 }
-
-                if (msg.startsWith("Olleh")) {
+                if (same(msg[0], 0x01)) {
                     runOnUiThread {
                         setContentView(R.layout.gamepad_activity)
-                        initGamepadButtons(send)
+                        gamepadLoop()
                     }
                 } else {
                     throw RException("Bad response, try an other client.")
@@ -150,12 +170,12 @@ class MainActivity : AppCompatActivity() {
             Thread {
                 run()
             }.start()
-        } catch (e: Exception) {}
+        } catch (e: Exception) {
+        }
     }
 
     override fun onStop() {
-        send?.close()
-        //receive?.close()
+        socket?.close()
         super.onStop()
     }
 }
