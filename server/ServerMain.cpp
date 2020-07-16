95cde4e0b8133f7109c2277d7861f97a2828b8dc
iElden <mceldenn@gmail.com>
2020-07-16 14:40:01 +0200

    Added onPacketReceived

--- server/ServerMain.cpp
+++ server/ServerMain.cpp
@@ -62,8 +62,48 @@ namespace RC::Server
 		client->connection.sendLobbyList(this->lobbies.getNLobbies());
 	}
 
+	void Main::onLobbyStateRequest(const std::shared_ptr<Client> &client)
+	{
+		Lobby &lobby = this->lobbies.getLobbyByClient(*client);
+		client->connection.sendLobbyState(lobby.getNPlayers());
+	}
+
 	void Main::onPacketReceived(const std::shared_ptr<Client> &client, const Network::Packet &packet)
 	{
 		assert(client.use_count() >= 2);
+
+		using namespace RC::Network;
+
+		printf("opcode %d\n", packet.header.code);
+		try {
+			switch (packet.header.code) {
+			case Opcode::GOODBYE:
+				client->connection.disconnect();
+				break;
+			case JOIN_LOBBY:
+				this->onJoinLobby(client, packet.joinLobby.id);
+				break;
+			case CREATE_LOBBY:
+				this->onCreateLobby(client);
+				break;
+			case DELETE_LOBBY:
+				this->onDeleteLobby(client);
+				break;
+			case LEAVE_LOBBY:
+				this->onLeaveLobby(client);
+				break;
+			case LOBBY_LIST_REQUEST:
+				this->onLobbyListRequest(client);
+				break;
+			case LOBBY_STATE_REQUEST:
+				this->onLobbyStateRequest(client);
+				break;
+			default:
+				throw InvalidOpcodeException(packet.header.code);
+			}
+		}
+		catch (ServerException &exc) {
+			client->connection.sendError(exc.what());
+		}
 	}
 }
